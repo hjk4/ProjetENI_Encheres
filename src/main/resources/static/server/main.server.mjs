2c614e64c96c90b582c25055a49848a4de66b883
hjk4 <ferreri.baptiste@gmail.com>
2024-09-13 10:33:21 +0200

    Version 1 ENI_Enchere | Angular cofigured as frontend and prebuilt

---
+++ src/main/resources/static/server/main.server.mjs
@@ -0,0 +1,10 @@
+import './polyfills.server.mjs';
+import {
+  main_server_default
+} from "./chunk-7RZKLDV3.mjs";
+import "./chunk-UE6FCGEB.mjs";
+import "./chunk-24VIC3GD.mjs";
+export {
+  main_server_default as default
+};
+//# sourceMappingURL=main.server.mjs.map
